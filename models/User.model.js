58e3a2d78063e9b6176f5e10f24b92ad3fe83bdd
pgtognoni <pgtognoni@gmail.com>
2023-03-01 13:47:10 +0000

    pachage.json dependencies

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,15 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: [true, "Please add a username"],
+      unique: [true, "That username is already taken"],
+      trim: true,
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
-      unique: true,
       lowercase: true,
       trim: true
     },
