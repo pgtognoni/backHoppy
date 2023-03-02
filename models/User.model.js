26b6fbf58a5356992f7e843156e9a85f303730b2
pgtognoni <pgtognoni@gmail.com>
2023-03-02 12:35:27 +0000

    changes in user model - added BIO

--- models/User.model.js
+++ models/User.model.js
@@ -13,6 +13,9 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Password is required.']
     },
+    bio: {
+      type: String,
+    },
     image: {
       type: String,
     },
